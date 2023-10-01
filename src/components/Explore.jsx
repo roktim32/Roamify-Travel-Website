8d3d923211e37b98c2533b263b85860d283560f6
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2023-10-01 14:23:44 +0530

    Completed the explore component

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const Explore = ({ title, placesAPI }) => {
     return (
         <>
-            <div className=''>
-                <div className=''>
-                    <div className=''>
-                        <h1 className=''>{title}</h1>
+            <div className='realtive my-7 md:mt-3'>
+                <div className='roamify-container'>
+                    <div className='flex items-center justify-center text-center mb-11 md:mb-7'>
+                        <h1 className='text-5xl lg:text-4xl md:text-3xl sm:text-2xl xsm:text-xl font-bold filter drop-shadow-lg text-slate-900'>{title}</h1>
                     </div>
-                    <div className=''>
+                    <div className='grid items-center grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-5'>
                         {placesAPI?.map((val, i) => (
-                            <div key={i} className=''>
-                                <div className=''>
-                                    <img src={val.placeImg} alt={val.location} className='' />
+                            <div key={i} className='flex items-center gap-5 sm:gap-3 rounded-lg hover:bg-emerald-300 transition-all duration-300 cursor-pointer hover:scale-105'>
+                                <div className='flex items-center justify-center'>
+                                    <img src={val.placeImg} alt={val.location} className='w-20 h-20 sm:h-16 sm:w-16 rounded-lg filter drop-shadow-lg ' />
                                 </div>
-                                <div className=''>
-                                    <h1>{val.location}</h1>
-                                    <p>{val.distance}</p>
+                                <div className='flex items-start flex-col text-slate-900'>
+                                    <h1 className='text-lg sm:text-sm font-bold'>{val.location}</h1>
+                                    <p className='font-normal lg:text-sm text-base sm:text-xs'>{val.distance}</p>
                                 </div>
                             </div>
                         ))}
