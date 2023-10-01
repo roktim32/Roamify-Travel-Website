e3bbe2b451170eae033f551fdb163db3866978d3
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2023-10-02 00:10:22 +0530

    Started Pricing Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Hero, Footer, Memory, Explore } from "./components"
+import { Navbar, Hero, Footer, Memory, Explore, Pricings } from "./components"
 import Advertise from "./components/Advertise"
 import { hero, navlinks, memory, placesAPI, brands } from "./data/roamifydata"
 
@@ -10,6 +10,7 @@ function App() {
       <Memory memory={memory} />
       <Explore title="Explore The Beautiful World" placesAPI={placesAPI} />
       <Advertise brands={brands} />
+      <Pricings />
       <Footer />
     </>
   )
---
+++ src/components/Pricings.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Pricings = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <div>
+                        <h1></h1>
+                        <p></p>
+                        <div>
+                            <button type='button'></button>
+                            <button type='button'></button>
+                        </div>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Pricings;
